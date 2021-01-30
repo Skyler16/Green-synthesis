5430d838e02d7f1777602b047fe6d62bb9bd6d20
Skyler16 <64692388+Skyler16@users.noreply.github.com>
2021-01-30 20:27:16 +0800

    整理

---
+++ Green-synthesis/Assets/Scroller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Scroller : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+
+    public ScrollRect scrollerRect;
+    public float moveSpeed = 0.5f; 
+
+    float horposition = 0;
+    float distance = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollerRect = GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        ((IEndDragHandler)scrollerRect).OnEndDrag(eventData);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ((IBeginDragHandler)scrollerRect).OnBeginDrag(eventData);
+    }
+}
